2039997bcf3cc8b25397d19119824356cba1b6ac
BRIAN BETT <+>
2023-06-06 16:31:33 +0300

    adding relationship to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,10 @@
 class Booking < ActiveRecord::Base
-end
+    belongs_to :user
+    belongs_to :car
+  
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :booking_status, presence: true
+  end
+  
