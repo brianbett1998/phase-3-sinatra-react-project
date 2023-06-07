11cf3a3d0a756f7ea9199ad29ea87a2b63493d9a
BRIAN BETT <+>
2023-06-07 11:20:57 +0300

    setting up app controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,30 @@
+require 'sinatra/base'
+require 'json'
+require_relative 'controllers/cars_controller'
+require_relative 'controllers/bookings_controller'
+require_relative 'controllers/users_controller'
+require_relative 'controllers/reviews_controller'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  # Cars routes
+  use CarsController
+
+  # Bookings routes
+  use BookingsController
+
+  # Users routes
+  use UsersController
+
+  # Reviews routes
+  use ReviewsController
+
+  # Root route
+  get '/' do
+    { message: "Welcome to my application!" }.to_json
   end
 
+  # Run the application
+  run! if app_file == $0
 end
